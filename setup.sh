8e983029f92030eeab04e62387e39a73e23cb1a6
Jim Buck <jim@jimbuck.io>
2021-08-24 17:31:44 -0400

    App setup.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 npx create-nx-workspace dash --name dash --preset empty --nx-cloud true --style scss
 cd dash
 
-npm i -D @nrwl/next
+npm i -D @nrwl/next @nrwl/express
 
-npx nx g @nrwl/next:app ???
-
+npx nx g @nrwl/next:app ui
+npx nx g @nrwl/express:application server --frontendProject=ui
