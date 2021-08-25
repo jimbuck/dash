de81c060bf81b65afe29451ba16656fbb2299ea4
Jim Buck <jim@jimbuck.io>
2021-08-24 21:44:46 -0400

    Added electron app.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 npx create-nx-workspace dash --name dash --preset empty --nx-cloud true --style scss
 cd dash
 
-npm i -D @nrwl/next @nrwl/express
+npm i -D @nrwl/next @nrwl/express nx-electron
 
 npx nx g @nrwl/next:app ui
 npx nx g @nrwl/express:application server --frontendProject=ui
+npx nx g nx-electron:app desktop --frontendProject=ui
