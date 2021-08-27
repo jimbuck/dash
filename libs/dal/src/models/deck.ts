a9319dfd2fcb515b3ee18ede5e0bc3bfe4e73128
Jim Buck <jim@jimbuck.io>
2021-08-27 15:26:31 -0400

    Cleaned up Deck model.

--- libs/dal/src/models/deck.ts
+++ libs/dal/src/models/deck.ts
@@ -3,10 +3,10 @@ import { ObjectType, Field } from 'type-graphql';
 @ObjectType()
 export class Deck {
 
-	@Field() public id: string;
-	@Field() public name: string;
-	@Field({ nullable: true }) public description?: string;
-	@Field(() => Date) public lastUpdated: Date;
+	@Field({ description: 'The unique identifier of the deck.' }) public id: string;
+	@Field({ description: 'The display name of the deck.' }) public name: string;
+	@Field({ nullable: true, description: 'The detailed description of the deck.' }) public description?: string;
+	@Field(() => Date, { description: 'The timestamp of the last time the deck was updated. Defaults to the creation time.' }) public lastUpdated: Date;
 
 	constructor(props: Deck) {
 		Object.assign(this, props);
